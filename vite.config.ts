ca3b109a359f1d6a1ba5a061184bb7d2ca1bd2c7
Jack Herrington <jherr@pobox.com>
2024-05-01 21:16:59 -0700

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/depth-of-field/",
   plugins: [react()],
-})
+});
